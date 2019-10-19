ca0977cf1c5e722608f5ba892ca7f75078a200db
Evan C Colwell <ecc108@uxb4.wiu.edu>
2019-10-18 19:52:24 -0500

    Java Golf

---
+++ golf.java
@@ -0,0 +1,1 @@
+public class golf{public static void main(String[] args){int f0, f1, temp;f0=1;f1=1;temp=0;System.out.println(f0+"\n"+f1);for(int i=0;i<25;i++){temp=f1+f0;f0=f1;f1=temp;System.out.println(f1);}}}
